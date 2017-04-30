147a0b9c1f28f3c1be637f81a64f92f9c3ba6a16
Daniel <omnisurfer@gmail.com>
2017-04-30 14:50:41 -0700

    start adding EJB

--- src/java/controller/ControllerServlet.java
+++ src/java/controller/ControllerServlet.java
@@ -6,12 +6,13 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import session.CategoryFacade;
 
 /**
  *
@@ -27,6 +28,9 @@ import javax.servlet.http.HttpServletResponse;
                            "/purchase",
                            "/chooseLanguage"})
 public class ControllerServlet extends HttpServlet {
+    
+    @EJB
+    private CategoryFacade categoryFacade;
 
     /**
      * Handles the HTTP <code>GET</code> method.
